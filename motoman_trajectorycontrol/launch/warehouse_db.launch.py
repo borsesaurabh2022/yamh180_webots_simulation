dbb073f96f4a6c105e45c157480a48de85372b7f
saurabh borse <saurabh.borse@alumni.fh-aachen.de>
2024-11-17 21:02:34 +0100

    cleanup: restructured packages and deleted unnecessary pkgs

--- motoman_trajectorycontrol/launch/warehouse_db.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_warehouse_db_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("motoman_mh_180_120", package_name="motoman_moveitcontrol").to_moveit_configs()
-    return generate_warehouse_db_launch(moveit_config)
--- yaskawa_motomanmh180_moveit/launch/demo.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_demo_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("motoman_mh_180_120", package_name="yaskawa_motomanmh180_moveit").to_moveit_configs()
-    return generate_demo_launch(moveit_config)
